48feaad1cd44e7f444ce2bb2301e6c2798134b4f
Fernando Salom <fer@rudo.es>
2023-06-06 11:13:41 +0200

    Add initial project

---
+++ microworkout/microworkoutApp.swift
@@ -0,0 +1,20 @@
+//
+//  microworkoutApp.swift
+//  microworkout
+//
+//  Created by Fernando Salom Carratala on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct microworkoutApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
