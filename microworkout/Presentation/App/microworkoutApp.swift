5b556922d97845f3066594d749cd8acf31cf08c4
Fernando Salom <fer@rudo.es>
2025-02-06 16:58:09 +0100

    Remove persistent controller

--- microworkout/Presentation/App/microworkoutApp.swift
+++ microworkout/Presentation/App/microworkoutApp.swift
@@ -1,20 +1,11 @@
-//
-//  microworkoutApp.swift
-//  microworkout
-//
-//  Created by Fernando Salom Carratala on 2/6/23.
-//
-
 import SwiftUI
 
 @main
 struct microworkoutApp: App {
-    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {            
             SplashView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
