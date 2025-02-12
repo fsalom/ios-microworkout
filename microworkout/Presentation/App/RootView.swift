5b1d9932935e0884003bffaf2f0da46ea79ad373
Fernando Salom <fer@rudo.es>
2025-02-12 17:40:56 +0100

    Add root view with main stack view

---
+++ microworkout/Presentation/App/RootView.swift
@@ -0,0 +1,32 @@
+//
+//  RootView.swift
+//  microworkout
+//
+//  Created by Fernando Salom Carratala on 11/2/25.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    @State var navigator: NavigatorProtocol
+    let rootTransition: AnyTransition = .opacity
+
+    public init(navigator: NavigatorProtocol = Navigator.shared, root: any View) {
+        self.navigator = navigator
+        navigator.initialize(root: root)
+    }
+
+    public var body: some View {
+        ZStack {
+            if let root = navigator.root {
+                StackView(root: {
+                    root
+                })
+            }
+        }
+    }
+}
+
+#Preview {
+    RootView(root: EmptyView())
+}
