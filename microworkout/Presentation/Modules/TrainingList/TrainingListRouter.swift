c58bdebe51b11db881489ed4a2fc74c70df5624b
Fernando Salom <fer@rudo.es>
2025-02-13 10:20:12 +0100

    Create TrainingListRouter.swift

---
+++ microworkout/Presentation/Modules/TrainingList/TrainingListRouter.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+class TrainingListRouter {
+    private var navigator: NavigatorProtocol
+
+    init(navigator: NavigatorProtocol) {
+        self.navigator = navigator
+    }
+
+    func goToWorkoutList() {
+        navigator.push(to: TrainingDetailBuilder().build())
+    }
+}
