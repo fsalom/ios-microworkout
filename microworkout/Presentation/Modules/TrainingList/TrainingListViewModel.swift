6bef3e200fd70ac83abe9896ff0959f17087dfda
Fernando Salom <fer@rudo.es>
2025-02-13 10:20:03 +0100

    Update TrainingListViewModel.swift

--- microworkout/Presentation/Modules/TrainingList/TrainingListViewModel.swift
+++ microworkout/Presentation/Modules/TrainingList/TrainingListViewModel.swift
@@ -1,11 +1,43 @@
 import Foundation
 
 final class TrainingListViewModel: ObservableObject {
+    enum TypeOfList {
+        case horizontal
+        case vertical
+    }
+
     @Published var trainings: [Training] = []
+    @Published var typeOfList: TypeOfList = .horizontal
 
+    private var router: TrainingListRouter
     
+    init(router: TrainingListRouter) {
+        self.router = router
+        self.loadTrainings()
+    }
 
-    init() { }
 
+    private func loadTrainings() {
+        Task {
+            await MainActor.run {
+                trainings = [
+                    Training(name: "Flexiones", image: "push-up-1",type: .strength, numberOfSets: 10, numberOfReps: 10),
+                    Training(name: "Dominadas", image: "pull-up-1", type: .strength, numberOfSets: 10, numberOfReps: 5),
+                    Training(name: "Sentadillas", image: "squat-1", type: .strength, numberOfSets: 10, numberOfReps: 20)
+                ]
+            }
+        }
+    }
 
+    func goToWorkout() {
+        router.goToWorkoutList()
+    }
+
+    func changeListType() {
+        Task {
+            await MainActor.run {
+                typeOfList = typeOfList == .horizontal ? .vertical : .horizontal
+            }
+        }
+    }
 }
