a185e77198e698097f844c00cb213f5649339538
Fernando Salom <fer@rudo.es>
2025-02-14 09:51:22 +0100

    Add dynamic grid to indicate days of training and minutes

---
+++ microworkout/Presentation/Modules/TrackingDay/Components/DynamicGridView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct DynamicGridView: View {
+    @State private var columnCount: Int
+    @State private var rowCount: Int
+    @State private var selection: Int = 0
+
+    private let colors: [Color] = [.red, .blue, .green, .yellow, .orange, .purple, .pink, .gray, .cyan]
+
+    var gridItems: [GridItem] {
+        Array(repeating: GridItem(.flexible(), spacing: 10), count: columnCount)
+    }
+
+    var body: some View {
+        VStack {
+            Picker("", selection: $selection) {
+                Text("Semanal").tag(0)
+                Text("Quincenal").tag(1)
+            }
+            .pickerStyle(.segmented)
+            .onChange(of: selection) { newValue, oldValue in
+                switch newValue {
+                case 0:
+                    columnCount = 7
+                    rowCount = 4
+                default:
+                    columnCount = 14
+                    rowCount = 4
+                }
+            }
+
+            LazyVGrid(columns: gridItems, spacing: 5) {
+                ForEach(0..<(columnCount * rowCount), id: \.self) { index in
+                    RoundedRectangle(cornerRadius: 5)
+                        .fill(colors[index % colors.count])
+                        .frame(height: 20)
+                }
+            }
+        }
+        .padding()
+        .animation(.default, value: columnCount)
+        .animation(.default, value: rowCount)
+    }
+}
