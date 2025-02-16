04195c84bc9d0e168568c6964636af8ad32a76d3
Fernando Salom <fer@rudo.es>
2025-02-16 09:47:56 +0100

    Add home router

---
+++ microworkout/Presentation/Modules/Home/HomeRouter.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+class HomeRouter {
+    private var navigator: NavigatorProtocol
+
+    init(navigator: NavigatorProtocol) {
+        self.navigator = navigator
+    }
+
+    func goToWorkoutList() {
+        navigator.push(to: TrainingListBuilder().build())
+    }
+}
