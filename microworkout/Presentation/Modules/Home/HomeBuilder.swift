f56a668c774dd30ee666d3004141016db3c3741b
Fernando Salom <fer@rudo.es>
2025-02-18 13:06:29 +0100

    Add dependency health kit manager

--- microworkout/Presentation/Modules/Home/HomeBuilder.swift
+++ microworkout/Presentation/Modules/Home/HomeBuilder.swift
@@ -1,7 +1,7 @@
 
 class HomeBuilder {
     func build() -> HomeView {
-        let viewModel = HomeViewModel(router: HomeRouter(navigator: Navigator.shared))
+        let viewModel = HomeViewModel(router: HomeRouter(navigator: Navigator.shared), healthKitManager: HealthKitManager.shared)
         return HomeView(viewModel: viewModel)
     }
 }
