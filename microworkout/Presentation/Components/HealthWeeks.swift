405aa9aa89a9269da829e69835645eeb4b350c9a
Fernando Salom <fer@rudo.es>
2025-02-18 13:06:00 +0100

    Add component to draw all exercises min by day

---
+++ microworkout/Presentation/Components/HealthWeeks.swift
@@ -0,0 +1,62 @@
+//
+//  Weeks.swift
+//  microworkout
+//
+//  Created by Fernando Salom Carratala on 16/2/25.
+//
+
+import SwiftUI
+
+struct HealthWeeksView: View {
+
+    @Binding var weeks: [[HealthDay]]
+    let daysOfWeek = ["L", "M", "X", "J", "V", "S", "D"]
+    var maxMinutes: Int {
+        weeks.flatMap { $0.map { $0.minutesOfExercise } }.max() ?? 1
+    }
+
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd"
+        return formatter
+    }()
+
+    var body: some View {
+        VStack(spacing: 10) {
+            // Encabezado de días
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 7), spacing: 5) {
+                ForEach(daysOfWeek, id: \.self) { day in
+                    Text(day)
+                        .font(.headline)
+                        .frame(width: 40, height: 40)
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.horizontal)
+
+            ForEach(weeks.indices, id: \.self) { weekIndex in
+                LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 7), spacing: 5) {
+                    ForEach(weeks[weekIndex].indices, id: \.self) { dayIndex in
+                        let healthDay = weeks[weekIndex][dayIndex]
+                        let opacity = maxMinutes > 0 ? Double(healthDay.minutesOfExercise) / Double(maxMinutes) : 0.2
+
+                        Text("\(healthDay.date, formatter: dateFormatter)")
+                            .font(.caption)
+                            .fontWeight(Calendar.current.isDate(healthDay.date, inSameDayAs: Date()) ? .bold : .thin)
+                            .frame(width: 40, height: 40)
+                            .background(Color.green.opacity(opacity))
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(Color.green, lineWidth: Calendar.current.isDate(healthDay.date, inSameDayAs: Date()) ? 1 : 0)
+                            )
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HealthWeeksView(weeks: .constant([[]]))
+}
