0510d1f9ba3c1ca9bc216252e206a2001f934927
Fernando Salom <fer@rudo.es>
2025-02-22 14:11:49 +0100

    Add namespace

--- microworkout/Presentation/Modules/TrainingDetail/TrainingDetailBuilder.swift
+++ microworkout/Presentation/Modules/TrainingDetail/TrainingDetailBuilder.swift
@@ -1,7 +1,9 @@
+import SwiftUI
+
 class TrainingDetailBuilder {
-    func build() -> TrainingDetailView {
+    func build(this training: Training, and namespace: Namespace.ID) -> TrainingDetailView {
         let router = TrainingDetailRouter(navigator: Navigator.shared)
-        let viewModel = TrainingDetailViewModel(router: router)
+        let viewModel = TrainingDetailViewModel(router: router, training: training, namespace: namespace)
         return TrainingDetailView(viewModel: viewModel)
     }
 }
