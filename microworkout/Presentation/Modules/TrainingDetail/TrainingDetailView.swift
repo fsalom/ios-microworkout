f5690e371abf9dd35bc54fb78c775ebf63943ad1
Fernando Salom <fer@rudo.es>
2025-02-22 14:12:01 +0100

    Modify view

--- microworkout/Presentation/Modules/TrainingDetail/TrainingDetailView.swift
+++ microworkout/Presentation/Modules/TrainingDetail/TrainingDetailView.swift
@@ -5,15 +5,49 @@ struct TrainingDetailView: View {
     @ObservedObject var viewModel: TrainingDetailViewModel
 
     var body: some View {
-        Text("To be implemented")
-        SliderView(onFinish: {
+        VStack {
+            Image(viewModel.training.image)
+                .resizable()
+                .scaledToFill()
+                .frame(height: 300)
+                .clipped()
+                .matchedGeometryEffect(id: viewModel.training.image, in: viewModel.namespace, isSource: false)
+                .opacity(0.5)
+            Text(viewModel.training.name)
+                .font(.system(size: 50))
+                .fontWeight(.bold)
+            VStack(alignment: .leading, spacing: 8) {
+                Text("\(viewModel.training.numberOfSets) Series")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(.gray).opacity(0.5)
+                    .clipShape(RoundedRectangle(cornerRadius: 6))
+                Slider(value: $viewModel.training.numberOfSetsForSlider, in: 1...20, step: 1)
+                Text("\(viewModel.training.numberOfReps) Repeticiones")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(.gray).opacity(0.5)
+                    .clipShape(RoundedRectangle(cornerRadius: 6))
+                Slider(value: $viewModel.training.numberOfRepsForSlider, in: 1...20, step: 1)
+                Spacer()
+                SliderView(onFinish: {
 
-        }, isWaitingResponse: false)
+                }, isWaitingResponse: false)
+            }
+            .padding()
+        }
     }
 }
 
 struct TrainingDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        TrainingDetailBuilder().build()
+        @Namespace var namespace
+        TrainingDetailBuilder().build(this: Training(name: "example",
+                                                     image: "abs-1",
+                                                     type: .strength,
+                                                     numberOfSets: 10,
+                                                     numberOfReps: 10,
+                                                     numberOfMinutesPerSet: 60),
+                                      and: namespace)
     }
 }
