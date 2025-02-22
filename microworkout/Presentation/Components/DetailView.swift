789a627e9f9b631b06a79ba5ddd453da1be3dd96
Fernando Salom <fer@rudo.es>
2025-02-22 14:11:14 +0100

    Add new design

--- microworkout/Presentation/Components/DetailView.swift
+++ microworkout/Presentation/Components/DetailView.swift
@@ -4,71 +4,88 @@ struct DetailView: View {
     var animation: Namespace.ID
     @Binding var showDetail: Bool
     @Binding var training: Training
-    @State private var showContent: Bool = false // Asegurar que siempre comienza en false
 
     var body: some View {
-        ZStack(alignment: .topTrailing) {
-            VStack {
-                Image(training.image)
-                    .resizable()
-                    .frame(height: 300)
-                    .matchedGeometryEffect(id: training.image, in: animation, isSource: false)
-                    .clipped()
-                    .mask(RoundedRectangle(cornerRadius: 20.0))
-
-                if showContent {
-                    Text(training.name)
-                        .font(.system(size: 50))
-                        .fontWeight(.bold)
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("\(training.numberOfSets) Series")
-                            .padding()
-                            .frame(maxWidth: .infinity)
-                            .background(.gray).opacity(0.5)
-                            .clipShape(RoundedRectangle(cornerRadius: 6))
-                        Slider(value: $training.numberOfSetsForSlider, in: 1...20, step: 1)
-                        Text("\(training.numberOfReps) Repeticiones")
-                            .padding()
-                            .frame(maxWidth: .infinity)
-                            .background(.gray).opacity(0.5)
-                            .clipShape(RoundedRectangle(cornerRadius: 6))
-                        Slider(value: $training.numberOfRepsForSlider, in: 1...20, step: 1)
-                        Spacer()
-                        SliderView(onFinish: {}, isWaitingResponse: false)
-                    }
-                    .padding()
-                } else {
-                    Spacer()
+        VStack{
+            ZStack(alignment: .top) {
+                GeometryReader { geometry in
+                    Image(training.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: geometry.size.width, height: 300)
+                        .matchedGeometryEffect(id: training.image, in: animation, isSource: showDetail)
+                        .clipShape(RoundedRectangle(cornerRadius: 20.0)) // Aplica el redondeo correctamente
                 }
+                .frame(height: 300)
+                Text(training.name)
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .padding(.top, 150)
+                    .foregroundStyle(.white)
+                    .shadow(radius: 10)
+                dismissButton
+            }
+            VStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    getSlidersView()
+                    getTextTotal()
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(Color.gray.opacity(0.2)) // Color de fondo
+                        )
+                    Spacer()
+                    SliderView(onFinish: {}, isWaitingResponse: false)
+                }
+                .padding(16)
             }
             .padding(.bottom, 60)
-            dismissButton
         }
         .navigationBarBackButtonHidden()
         .edgesIgnoringSafeArea(.all)
-        .onAppear {
-            // Resetear el contenido antes de animar
-            showContent = false
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                showContent = true
-            }
-        }
+    }
+
+    @ViewBuilder
+    func getTextTotal() -> Text {
+        Text("Así harás un total de ") +
+        Text("\(training.numberOfReps*training.numberOfSets)").fontWeight(.bold) +
+        Text(" repeticiones a lo largo de ") +
+        Text("\(Int(training.numberOfMinutesPerSet*training.numberOfSets)/60)").fontWeight(.bold) +
+        Text(" horas (aproximadamente)")
+
+    }
+
+    @ViewBuilder
+    func getSlidersView() -> some View {
+        Text("\(training.numberOfSets) series")
+            .fontWeight(.bold)
+        Slider(value: $training.numberOfSetsForSlider, in: 1...20, step: 1)
+        Divider()
+        Text("\(training.numberOfReps) repeticiones")
+            .fontWeight(.bold)
+        Slider(value: $training.numberOfRepsForSlider, in: 1...20, step: 1)
+        Divider()
+        Text("\(training.numberOfMinutesPerSet) minutos entre series")
+            .fontWeight(.bold)
+        Slider(value: $training.numberOfMinutesPerSetForSlider, in: 10...120, step: 10)
     }
 
     var dismissButton: some View {
-        Button {
-            showContent = false // Resetear contenido antes de cerrar la vista
-            withAnimation(.smooth) {
-                showDetail = false
+        HStack {
+            Spacer()
+            Button {
+                withAnimation(.smooth) {
+                    showDetail = false
+                }
+            } label: {
+                Image(systemName: "xmark")
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(Color(uiColor: .black))
+                    .background(Color(uiColor: .white))
+                    .clipShape(Circle())
+                    .padding(.top, 60)
+                    .padding(.trailing, 40)
             }
-        } label: {
-            Image(systemName: "xmark")
-                .frame(width: 30, height: 30)
-                .foregroundColor(Color(uiColor: .black))
-                .background(Color(uiColor: .white))
-                .clipShape(Circle())
-                .padding(.top, 60)
-                .padding(.trailing, 40)
         }
     }
 }
