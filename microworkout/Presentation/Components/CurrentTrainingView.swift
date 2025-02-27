1e3d097ba3990160afe4f2286a65385aebdd6e13
Fernando Salom <fer@rudo.es>
2025-02-27 12:54:17 +0100

    Update CurrentTrainingView.swift

--- microworkout/Presentation/Components/CurrentTrainingView.swift
+++ microworkout/Presentation/Components/CurrentTrainingView.swift
@@ -9,19 +9,21 @@ import SwiftUI
 
 struct CurrentTrainingView: View {
     @Binding var isPresented: Bool
-    @Binding var training: Training
+    @Binding var training: Training?
+    @State var hasToResetTimer = false
+    @State private var isPressed = false
     var animation: Namespace.ID
 
     var body: some View {
         ZStack(alignment: .top) {
             Color.blue
-            .matchedGeometryEffect(id: "background", in: animation)
-            .ignoresSafeArea()
-            .onTapGesture {
-                withAnimation(.spring()) {
-                    isPresented = false
+                .matchedGeometryEffect(id: "background", in: animation)
+                .ignoresSafeArea()
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        isPresented = false
+                    }
                 }
-            }
             VStack{
                 getTextTotal()
                     .padding()
@@ -30,9 +32,10 @@ struct CurrentTrainingView: View {
                         RoundedRectangle(cornerRadius: 15)
                             .fill(Color.white)
                     )
+                    .padding(.bottom, 50)
                 HStack(spacing: 10) {
                     VStack {
-                        Text("\(training.numberOfSets)")
+                        Text("\(training!.numberOfSetsCompleted)")
                             .font(.title)
                             .foregroundStyle(.white)
                             .fontWeight(.bold)
@@ -42,7 +45,7 @@ struct CurrentTrainingView: View {
                     }
                     .frame(maxWidth: .infinity)
                     VStack {
-                        Text("\(training.numberOfReps)")
+                        Text("\(training!.numberOfSetsCompleted*training!.numberOfReps)")
                             .font(.title)
                             .foregroundStyle(.white)
                             .fontWeight(.bold)
@@ -52,7 +55,7 @@ struct CurrentTrainingView: View {
                     }
                     .frame(maxWidth: .infinity)
                     VStack {
-                        Text("\(training.numberOfMinutesPerSet) min")
+                        Text(training!.startedAt!, style: .timer)
                             .font(.title)
                             .foregroundStyle(.white)
                             .fontWeight(.bold)
@@ -63,11 +66,50 @@ struct CurrentTrainingView: View {
                     .frame(maxWidth: .infinity)
                 }
                 .padding(.bottom, 100)
-                CountdownView(minutes: 60)
+                Button {
+                    training?.numberOfSetsCompleted += 1
+                    hasToResetTimer = true
+                    withAnimation(.easeOut(duration: 0.2)) {
+                        isPressed = true
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                        withAnimation {
+                            isPressed = false
+                        }
+                    }
+                } label: {
+                    CountdownView(startDate:training?.sets.last ?? Date(), totalMinutes: training!.numberOfMinutesPerSet, hasToResetTimer: $hasToResetTimer)
+                        .padding(5)
+                        .background(isPressed ? Color.blue : Color.clear)
+                        .overlay(
+                            Circle()
+                                .fill(Color.clear)
+                                .frame(width: 200, height: 200)
+                        )
+                }
+                .buttonStyle(.plain)
+                .padding(.bottom, 100)
+                Text("Pulsa al completar la serie")
+                    .font(.footnote)
+                    .foregroundStyle(.white)
+                Spacer()
+                SliderView(
+                    message: "Finalizar",
+                    backgroundColor: .white,
+                    frontColor: .blue,
+                    onFinish: {
+                        withAnimation {
+                            isPresented = false
+                        }
+                    },
+                    isWaitingResponse: false
+                )
+                .foregroundStyle(.white)
+                .padding(.bottom, 60)
             }
             .padding(.top, 100)
             .padding()
-            dismissButton
+            //dismissButton
         }
         .edgesIgnoringSafeArea(.all)
     }
@@ -75,9 +117,9 @@ struct CurrentTrainingView: View {
     @ViewBuilder
     func getTextTotal() -> Text {
         Text("Tu entreno actual consiste en un total de ") +
-        Text("\(training.numberOfReps*training.numberOfSets)").fontWeight(.bold) +
+        Text("\(training!.numberOfReps*training!.numberOfSets)").fontWeight(.bold) +
         Text(" repeticiones a lo largo de ") +
-        Text("\(Int(training.numberOfMinutesPerSet*training.numberOfSets)/60)").fontWeight(.bold) +
+        Text("\(Int(training!.numberOfMinutesPerSet*training!.numberOfSets)/60)").fontWeight(.bold) +
         Text(" horas")
     }
 
