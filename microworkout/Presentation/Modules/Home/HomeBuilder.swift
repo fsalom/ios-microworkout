72d1e9f08e503b199680e4e6a3c175575bd911b5
Fernando Salom <fer@rudo.es>
2025-03-01 21:41:59 +0100

    Update HomeBuilder.swift

--- microworkout/Presentation/Modules/Home/HomeBuilder.swift
+++ microworkout/Presentation/Modules/Home/HomeBuilder.swift
@@ -1,7 +1,10 @@
 
 class HomeBuilder {
     func build() -> HomeView {
-        let viewModel = HomeViewModel(router: HomeRouter(navigator: Navigator.shared), healthKitManager: HealthKitManager.shared)
+        let viewModel = HomeViewModel(
+            router: HomeRouter(navigator: Navigator.shared),
+            trainingUseCase: TrainingContainer().makeUseCase(),
+            healthKitManager: HealthKitManager.shared)
         return HomeView(viewModel: viewModel)
     }
 }
