bdbec6dea04bb750508ee5622896b89c19665456
Fernando Salom <fer@rudo.es>
2025-04-03 21:49:14 +0200

    Add function for health datasource

--- microworkout/Data/Datasources/Health/HealthKitDataSourceProtocol.swift
+++ microworkout/Data/Datasources/Health/HealthKitDataSourceProtocol.swift
@@ -1,12 +1,7 @@
-//
-//  HealthKitDataSourceProtocol.swift
-//  microworkout
-//
-//  Created by Fernando Salom Carratala on 22/11/23.
-//
-
 import Foundation
 
 protocol HealthKitDataSourceProtocol {
-
+    func requestAuthorization() async throws -> Bool
+    func fetchExerciseTimeToday() async throws -> Double?
+    func fetchExerciseTime(startDate: Date, endDate: Date) async throws -> [Date: Double]?
 }
