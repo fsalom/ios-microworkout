7237dc7b063519f7865fc9921b09546f0b328c3d
Fernando Salom <fer@rudo.es>
2025-04-04 23:38:12 +0200

    Move protocol to repository folder in domain

--- microworkout/Domain/Repositories/HealthRepositoryProtocol.swift
+++ microworkout/Domain/Repositories/HealthRepositoryProtocol.swift
@@ -4,4 +4,6 @@ protocol HealthRepositoryProtocol {
     func requestAuthorization() async throws -> Bool
     func fetchExerciseTimeToday() async throws -> Double?
     func fetchExerciseTime(startDate: Date, endDate: Date) async throws -> [Date: Double]?
+    func fetchStepsCountToday() async throws -> Double?
+    func fetchHoursStandingCount() async throws -> Double?
 }
