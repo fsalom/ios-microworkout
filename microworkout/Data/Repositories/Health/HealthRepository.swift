4c2b45033764f6ae456dae260202846fc7fe7862
Fernando Salom <fer@rudo.es>
2025-04-05 10:00:55 +0200

    Add new functions to get steps and stand hours

--- microworkout/Data/Repositories/Health/HealthRepository.swift
+++ microworkout/Data/Repositories/Health/HealthRepository.swift
@@ -18,4 +18,12 @@ class HealthRepository: HealthRepositoryProtocol {
     func fetchExerciseTime(startDate: Date, endDate: Date) async throws -> [Date : Double]? {
         try await dataSource.fetchExerciseTime(startDate: startDate, endDate: endDate)
     }
+    
+    func fetchStepsCountToday() async throws -> Double? {
+        try await dataSource.fetchStepsCountToday()
+    }
+
+    func fetchHoursStandingCount() async throws -> Double? {
+        try await dataSource.fetchHoursStandingCount()
+    }
 }
