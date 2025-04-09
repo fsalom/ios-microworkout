22ac4b1e75b2344ccb83d66555398d8218d79dc5
Fernando Salom <fer@rudo.es>
2025-04-09 14:05:43 +0200

    Add new auto calculated property to show date formatted

--- microworkout/Domain/Entities/HealthDay.swift
+++ microworkout/Domain/Entities/HealthDay.swift
@@ -6,4 +6,15 @@ struct HealthDay: Identifiable {
     var minutesOfExercise: Int = 0
     var steps: Int = 0
     var minutesStanding: Int = 0
+    var dateWithFormat: String {
+        let calendar = Calendar.current
+        if calendar.isDateInToday(date) {
+            return "Hoy"
+        }
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "es_ES")
+        dateFormatter.dateFormat = "d 'de' MMMM 'de' yyyy"
+        return dateFormatter.string(from: date)
+    }
 }
