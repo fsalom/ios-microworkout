5871483f387b6598386a3c24a37fe8793c5a609c
Fernando Salom <fer@rudo.es>
2025-04-13 21:24:35 +0200

    Add new logic to save and retrieve trainings

--- microworkout/Presentation/Modules/TrainingDetailV2/TrainingDetailV2ViewModel.swift
+++ microworkout/Presentation/Modules/TrainingDetailV2/TrainingDetailV2ViewModel.swift
@@ -1,15 +1,34 @@
 import Foundation
 import SwiftUI
 
+struct TrainingDetailUIState {
+    var currentTraining: Training?
+    var hasTrainingStarted: Bool = false
+}
+
 final class TrainingDetailV2ViewModel: ObservableObject {
 
     var namespace: Namespace.ID
     private var router: TrainingDetailV2Router
+    private var trainingUseCase: TrainingUseCase
     @Published var training: Training
+    @Published var uiState: TrainingDetailUIState = .init()
 
-    init(router: TrainingDetailV2Router, training: Training, namespace: Namespace.ID) {
+    init(trainingUseCase: TrainingUseCase,
+         router: TrainingDetailV2Router,
+         training: Training,
+         namespace: Namespace.ID) {
+        self.trainingUseCase = trainingUseCase
         self.router = router
-        self.training = training
         self.namespace = namespace
+        self.training = training
+    }
+
+    func startTraining() {
+        DispatchQueue.main.async {
+            self.trainingUseCase.save(self.training)
+            self.uiState.hasTrainingStarted = true
+            self.uiState.currentTraining = self.training
+        }
     }
 }
