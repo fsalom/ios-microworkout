397c37a0f907ba03ae944ba8cf9f0ec70786af6e
Fernando Salom <fer@rudo.es>
2025-04-14 22:05:06 +0200

    Update training detail builder with viewmodel and use case

--- microworkout/Presentation/Modules/TrainingDetailV2/TrainingDetailV2Builder.swift
+++ microworkout/Presentation/Modules/TrainingDetailV2/TrainingDetailV2Builder.swift
@@ -3,7 +3,11 @@ import SwiftUI
 class TrainingDetailV2Builder {
     func build(this training: Training, and namespace: Namespace.ID) -> TrainingDetailV2View {
         let router = TrainingDetailV2Router(navigator: Navigator.shared)
-        let viewModel = TrainingDetailV2ViewModel(router: router, training: training, namespace: namespace)
+        let viewModel = TrainingDetailV2ViewModel(
+            trainingUseCase: TrainingContainer().makeUseCase(),
+            router: router,
+            training: training,
+            namespace: namespace)
         return TrainingDetailV2View(viewModel: viewModel)
     }
 }
