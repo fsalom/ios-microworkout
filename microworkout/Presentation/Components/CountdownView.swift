ed9926dd31a86a3db44246f34716be9e552f5236
Fernando Salom <fer@rudo.es>
2025-04-18 20:38:19 +0200

    Add option with different action for round and finish

--- microworkout/Presentation/Components/CountdownView.swift
+++ microworkout/Presentation/Components/CountdownView.swift
@@ -3,11 +3,14 @@ import Combine
 
 struct CountdownButtonView: View {
     @Binding var hasToResetTimer: Bool
+    @Binding var sets: [Date]
+
     let startDate: Date
     let totalMinutes: Int
     let limitOfSets: Int
-    @Binding var sets: [Date]
+    
     let action: () -> Void
+    let end: () -> Void
 
     @State private var remainingTime: Int = 0
     @State private var timerSubscription: Cancellable?
@@ -19,46 +22,53 @@ struct CountdownButtonView: View {
 
     var body: some View {
         Button {
-            action()
+            if limitOfSets == sets.count {
+                end()
+            } else {
+                action()
+            }
             animatePress()
         } label: {
             ZStack{
                 Circle()
-                    .fill(isPressed ? Color.blue.opacity(0.5) : Color.white.opacity(0.3))
+                    .fill(isPressed
+                          ? Color.white.opacity(0.8) : Color.white.opacity(0.3)
+                    )
                     .frame(width: 200, height: 200)
-                if remainingTime > 0 {
-                    VStack{
-                        Text(timeFormatted(remainingTime))
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                        if limitOfSets - 1 == sets.count {
-                            Text("ÚLTIMA RONDA")
-                                .font(.footnote)
-                                .fontWeight(.black)
-                        }
-                    }
-                    .foregroundColor((limitOfSets - 1 == sets.count) ? .blue : .white)
-                    .frame(width: 180, height: 180)
-                    .background(Circle().fill((limitOfSets - 1 == sets.count) ? .white : .blue.opacity(0.3)))
-                } else {
-                    Text("TIEMPO AGOTADO")
+                if (limitOfSets == sets.count) ||
+                    (sets.count == (limitOfSets - 1) && remainingTime == 0) {
+                    Text("FIN")
                         .font(.title)
                         .fontWeight(.bold)
                         .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .frame(width: 180, height: 180)
-                        .background(
-                            Circle()
-                                .fill(
-                                    LinearGradient(
-                                        gradient: Gradient(colors: [Color.blue.opacity(0.7), Color.blue.opacity(0.3)]),
-                                        startPoint: .topLeading,
-                                        endPoint: .bottomTrailing
-                                    )
-                                )
-                                .shadow(color: Color.white.opacity(0.6), radius: 4, x: -4, y: -4) // luz superior izquierda
-                                .shadow(color: Color.black.opacity(0.2), radius: 4, x: 4, y: 4)   // sombra inferior derecha
-                        )
+                } else {
+                    VStack{
+                        if remainingTime > 0 {
+                            Text(timeFormatted(remainingTime))
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                            if limitOfSets - 1 == sets.count {
+                                Text("ÚLTIMA RONDA")
+                                    .font(.footnote)
+                                    .fontWeight(.black)
+                            }
+                        } else {
+                            Text("AGOTADO")
+                                .font(.title)
+                                .fontWeight(.black)
+                            Text("Pulsa para siguiente ronda")
+                                .font(.footnote)
+                        }
+                    }
+                    .foregroundColor(
+                        (limitOfSets - 1 == sets.count)
+                        ? .blue : .white
+                    )
+                    .frame(width: 180, height: 180)
+                    .background(Circle().fill(
+                        (limitOfSets - 1 == sets.count) ?
+                            .white : .blue.opacity(0.3))
+                    )
                 }
             }
         }
