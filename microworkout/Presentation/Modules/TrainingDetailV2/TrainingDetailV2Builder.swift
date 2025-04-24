93bde9f592bfc4e8449e50d3bcf90aefb6945498
Fernando Salom <fer@rudo.es>
2025-04-24 11:34:13 +0200

    Add app state replacing namespace

--- microworkout/Presentation/Modules/TrainingDetailV2/TrainingDetailV2Builder.swift
+++ microworkout/Presentation/Modules/TrainingDetailV2/TrainingDetailV2Builder.swift
@@ -1,13 +1,13 @@
 import SwiftUI
 
 class TrainingDetailV2Builder {
-    func build(this training: Training, and namespace: Namespace.ID) -> TrainingDetailV2View {
+    func build(this training: Training, and appState: AppState) -> TrainingDetailV2View {
         let router = TrainingDetailV2Router(navigator: Navigator.shared)
         let viewModel = TrainingDetailV2ViewModel(
             trainingUseCase: TrainingContainer().makeUseCase(),
             router: router,
             training: training,
-            namespace: namespace)
+            appState: appState)
         return TrainingDetailV2View(viewModel: viewModel)
     }
 }
