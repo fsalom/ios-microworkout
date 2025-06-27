b7e17b0b14e7b05d7bc0f817173862f1dbddd147
Fernando Salom <fer@rudo.es>
2025-06-27 13:37:00 +0200

    Add tabbar

--- microworkout/Presentation/Modules/Switcher/SwitcherView.swift
+++ microworkout/Presentation/Modules/Switcher/SwitcherView.swift
@@ -6,7 +6,24 @@ struct SwitcherView: View {
     var body: some View {
         switch appState.screen {
         case .home:
-            HomeBuilder().build(appState: appState)
+            TabView {
+                HomeBuilder().build(appState: appState)
+                    .navigationTitle("Inicio")
+                    .tabItem {
+                        Image(systemName: "house.circle.fill")
+                    }
+
+                HomeTrainingBuilder().build()
+                    .navigationTitle("Sesión")
+                    .tabItem {
+                        Image(systemName: "figure.strengthtraining.traditional.circle.fill")
+                    }
+                CurrentSessionView()
+                    .navigationTitle("Sesión")
+                    .tabItem {
+                        Image(systemName: "person.crop.circle.fill")
+                    }
+            }
         case .workout, .loading:
             ProgressView()
         }
