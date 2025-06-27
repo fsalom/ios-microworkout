d72c434e4d319494c839e50e5cbfee16f66a78b4
Fernando Salom <fer@rudo.es>
2025-06-27 13:37:11 +0200

    Add new view to start workout

---
+++ microworkout/Presentation/Modules/CurrrentSession/CurrentSessionView.swift
@@ -0,0 +1,18 @@
+//
+//  StartSessionView.swift
+//  microworkout
+//
+//  Created by Fernando Salom Carratala on 26/6/25.
+//
+
+import SwiftUI
+
+struct CurrentSessionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrentSessionView()
+}
