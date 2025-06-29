586479731da3eb4487b883b2e6f6d48d1c84b89f
Fernando Salom <fer@rudo.es>
2025-06-29 20:16:15 +0200

    Add crono view component

---
+++ microworkout/Presentation/Components/CronoView.swift
@@ -0,0 +1,31 @@
+//
+//  CronoView.swift
+//  microworkout
+//
+//  Created by Fernando Salom Carratala on 29/6/25.
+//
+
+import SwiftUICore
+
+
+struct CronoView: View {
+    @State private var startDate = Date() // Puedes cambiarlo a una fecha específica
+    @State private var elapsedTime: TimeInterval = 0
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
+    var body: some View {
+        Text(formattedTime(from: elapsedTime))
+            .font(.largeTitle)
+            .onReceive(timer) { _ in
+                elapsedTime = Date().timeIntervalSince(startDate)
+            }
+    }
+
+    private func formattedTime(from interval: TimeInterval) -> String {
+        let totalSeconds = Int(interval)
+        let hours = totalSeconds / 3600
+        let minutes = (totalSeconds % 3600) / 60
+        let seconds = totalSeconds % 60
+        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+    }
+}
