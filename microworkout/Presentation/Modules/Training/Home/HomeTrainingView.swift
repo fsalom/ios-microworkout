7b12ec3011c8999a69999d492d384aed27c811ff
Fernando Salom <fer@rudo.es>
2025-07-04 22:26:39 +0200

    Update HomeTrainingView.swift

--- microworkout/Presentation/Modules/Training/Home/HomeTrainingView.swift
+++ microworkout/Presentation/Modules/Training/Home/HomeTrainingView.swift
@@ -12,44 +12,23 @@ struct HomeTrainingView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-                    VStack(alignment: .leading, spacing: 10) {
-                        HStack(alignment: .top) {
-                            Image("splash")
-                                .resizable()
-                                .foregroundColor(.white)
-                                .frame(width: 60, height: 60)
-                                .clipShape(Circle())
-                            VStack(alignment: .leading, spacing: 2) {
-                                Text("Welcome back")
-                                    .font(.footnote)
-                                    .lineLimit(2)
-                                Text("Fernando!")
-                                    .font(.largeTitle)
-                                    .fontWeight(.bold)
-                            }
-                            Spacer()
-                            VStack(alignment: .leading) {
-                                Image(systemName: "bell")
-                            }
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Progresión de la semana")
+                        .font(.footnote)
+                    ProgressView(value: 0.4).progressViewStyle(.linear)
+                    ForEach($viewModel.workouts, id: \.id) { $workout in
+                        NavigationLink(destination:  DetailWorkoutBuilder().build(with: $workout)) {
+                            HomeWorkoutPlanView(plan: workout)
                         }
-
-
-                        Text("Progresión de la semana")
-                            .font(.footnote)
-                        ProgressView(value: 0.4).progressViewStyle(.linear)
-                        ForEach($viewModel.workouts, id: \.id) { $workout in
-                            NavigationLink(destination:  DetailWorkoutBuilder().build(with: $workout)) {
-                                HomeWorkoutPlanView(plan: workout)
-                            }
-                        }
-                    }.padding(16)
+                    }
+                }.padding(16)
             }.navigationTitle("Entrenamientos")
                 .navigationBarTitleDisplayMode(.inline)
         }
-            .onAppear {
-                viewModel.load()
-            }
-            .edgesIgnoringSafeArea(.all)
+        .onAppear {
+            viewModel.load()
+        }
+        .edgesIgnoringSafeArea(.all)
         //.scrollBounceBehavior(.basedOnSize)
     }
 }
