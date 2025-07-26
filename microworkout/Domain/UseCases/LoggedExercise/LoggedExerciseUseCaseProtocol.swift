efec39cc272b1832e084bf2081cdd0f5c02b4f68
Fernando Salom <fer@rudo.es>
2025-07-27 01:07:59 +0200

    Update LoggedExerciseUseCaseProtocol.swift

--- microworkout/Domain/UseCases/LoggedExercise/LoggedExerciseUseCaseProtocol.swift
+++ microworkout/Domain/UseCases/LoggedExercise/LoggedExerciseUseCaseProtocol.swift
@@ -5,7 +5,13 @@
 //  Created by Fernando Salom Carratala on 20/7/25.
 //
 
+import Foundation
+
 protocol LoggedExerciseUseCaseProtocol {
-    func add(this exercise: LoggedExercise) async throws
+    func add(new exercise: LoggedExercise) async throws
+    func update(this exercise: LoggedExercise)
+    func delete(with ids: [UUID])
     func getAll(for id: String) async throws -> [LoggedExercise]
+    func groupByExercise(these exercises: [LoggedExercise]) -> [Exercise: [LoggedExercise]]
+    func order(these exercises: [LoggedExercise]) -> [Exercise]
 }
