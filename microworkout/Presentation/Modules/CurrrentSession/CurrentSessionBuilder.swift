e0251cd165ec8f1cb43f8763b002829fe291ebee
Fernando Salom <fer@rudo.es>
2025-07-28 13:13:49 +0200

    Create CurrentSessionBuilder.swift

---
+++ microworkout/Presentation/Modules/CurrrentSession/CurrentSessionBuilder.swift
@@ -0,0 +1,14 @@
+
+
+class CurrentSessionBuilder {
+    func build() -> CurrentSessionView {
+        let loggedExerciseDataSource: LoggedExerciseDataSourceProtocol = LoggedExerciseLocalDataSource(localStorage: UserDefaultsManager())
+        let loggedExerciseRepository: LoggedExerciseRepositoryProtocol = LoggedExerciseRepository(dataSource: loggedExerciseDataSource)
+        let loggedExerciseUseCase: LoggedExerciseUseCaseProtocol = LoggedExerciseUseCase(repository: loggedExerciseRepository)
+        let mockExerciseDataSource: ExerciseDataSourceProtocol = ExerciseMockDataSource()
+        let exerciseRepository: ExerciseRepositoryProtocol = ExerciseRepository(dataSource: mockExerciseDataSource)
+        let exerciseUseCases: ExerciseUseCaseProtocol = ExerciseUseCase(repository: exerciseRepository)
+        let viewModel = CurrentSessionViewModel(exerciseUseCase: exerciseUseCases, loggedExerciseUseCase: loggedExerciseUseCase)
+        return CurrentSessionView(viewModel: viewModel)
+    }
+}
