3751b82dc73fe29c6bb03b4663fee1eacf13d604
Fernando Salom <fer@rudo.es>
2025-07-30 16:57:51 +0200

    Fix view model with new use cases

--- microworkout/Presentation/Modules/CurrrentSession/CurrentSessionViewModel.swift
+++ microworkout/Presentation/Modules/CurrrentSession/CurrentSessionViewModel.swift
@@ -10,16 +10,22 @@ import SwiftUI
 import Combine
 
 class CurrentSessionViewModel: ObservableObject {
-    @Published var searchText: String = ""
+    @Published var searchText: String = "" {
+        didSet {
+            search(with: self.searchText)
+        }
+    }
     @Published var loggedExercises: [LoggedExercise] = []
+    @Published var exercises: [Exercise] = []
     @Published var isRunning: Bool = false
     @Published var startTime: Date? = nil
     @Published var now: Date = Date()
     @Published var activeForm: ActiveExerciseForm?
+
     private var exerciseUseCase: ExerciseUseCaseProtocol
     private var loggedExerciseUseCase: LoggedExerciseUseCaseProtocol
 
-    init(exerciseUseCase: ExerciseUseCase, loggedExerciseUseCase: LoggedExerciseUseCaseProtocol) {
+    init(exerciseUseCase: ExerciseUseCaseProtocol, loggedExerciseUseCase: LoggedExerciseUseCaseProtocol) {
         self.exerciseUseCase = exerciseUseCase
         self.loggedExerciseUseCase = loggedExerciseUseCase
     }
@@ -28,7 +34,7 @@ class CurrentSessionViewModel: ObservableObject {
         case new(Exercise)
         case edit(LoggedExercise)
 
-        var id: UUID {
+        var id: String {
             switch self {
             case .new(let exercise): return exercise.id
             case .edit(let logged): return logged.id
@@ -36,21 +42,9 @@ class CurrentSessionViewModel: ObservableObject {
         }
     }
 
-    let exercises: [Exercise] = [
-        Exercise(name: "Press de banca"),
-        Exercise(name: "Sentadilla"),
-        Exercise(name: "Peso muerto"),
-        Exercise(name: "Dominadas"),
-        Exercise(name: "Press militar"),
-        Exercise(name: "Curl de bíceps"),
-        Exercise(name: "Remo con barra")
-    ]
-
-    var filteredExercises: [Exercise] {
-        if searchText.isEmpty {
-            return []
-        } else {
-            return exercises.filter { $0.name.lowercased().contains(searchText.lowercased()) }
+    func search(with text: String) {
+        Task { @MainActor in
+            self.exercises = try! await self.exerciseUseCase.getExercises(contains: self.searchText)
         }
     }
 
@@ -78,21 +72,21 @@ class CurrentSessionViewModel: ObservableObject {
         }
     }
 
-    func deleteExercises(with ids: [UUID]) {
+    func deleteExercises(with ids: [String]) {
         loggedExercises.removeAll { ids.contains($0.id) }
     }
 
-    func toggleCompletion(for exerciseId: UUID) {
+    func toggleCompletion(for exerciseId: String) {
         if let index = loggedExercises.firstIndex(where: { $0.id == exerciseId }) {
             loggedExercises[index].isCompleted.toggle()
         }
     }
 
     func groupedByExercise() -> [Exercise: [LoggedExercise]] {
-        Dictionary(grouping: loggedExercises, by: { $0.exercise })
+        self.loggedExerciseUseCase.groupByExercise(these: self.loggedExercises)
     }
 
     func orderedExercises() -> [Exercise] {
-        loggedExercises.map { $0.exercise }.uniqued()
+        self.loggedExerciseUseCase.order(these: self.loggedExercises)
     }
 }
