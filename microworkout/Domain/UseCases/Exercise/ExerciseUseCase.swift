3eb8b8b9c1fd4a0fa7c6f7a952c495fa36c24650
Fernando Salom <fer@rudo.es>
2025-08-01 23:44:42 +0200

    Fix exercise use case to include create and get all

--- microworkout/Domain/UseCases/Exercise/ExerciseUseCase.swift
+++ microworkout/Domain/UseCases/Exercise/ExerciseUseCase.swift
@@ -5,17 +5,21 @@
 //  Created by Fernando Salom Carratala on 20/7/25.
 //
 
-class ExerciseUseCase: ExerciseUseCaseProtocol {
+import Foundation
 
-    
+class ExerciseUseCase: ExerciseUseCaseProtocol {
     private var repository: ExerciseRepositoryProtocol
 
     init(repository: ExerciseRepositoryProtocol) {
         self.repository = repository
     }
 
-    func getExercises(contains searchText: String) async throws -> [Exercise] {
+    func getAll(contains searchText: String) async throws -> [Exercise] {
         let exercises = try await self.repository.getExercises()
         return exercises.filter { $0.name.lowercased().contains(searchText.lowercased()) }
+    }
+
+    func create(with name: String) async throws -> Exercise {
+        return Exercise(id: UUID().uuidString, name: name)
     }
 }
