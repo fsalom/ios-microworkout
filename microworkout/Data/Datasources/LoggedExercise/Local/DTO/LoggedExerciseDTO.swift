d9f9f374afdb3ff42a42c139314182d127f97d35
Fernando Salom <fer@rudo.es>
2025-08-02 21:45:14 +0200

    Add LoggedExerciseByDay

--- microworkout/Data/Datasources/LoggedExercise/Local/DTO/LoggedExerciseDTO.swift
+++ microworkout/Data/Datasources/LoggedExercise/Local/DTO/LoggedExerciseDTO.swift
@@ -5,6 +5,8 @@
 //  Created by Fernando Salom Carratala on 27/7/25.
 //
 
+import Foundation
+
 
 struct LoggedExerciseDTO: Codable, Equatable {
     static func == (lhs: LoggedExerciseDTO, rhs: LoggedExerciseDTO) -> Bool {
@@ -16,4 +18,21 @@ struct LoggedExerciseDTO: Codable, Equatable {
     var reps: Int
     var weight: Double
     var isCompleted: Bool = false
+}
+
+
+struct LoggedExerciseByDayDTO: Codable, Equatable {
+    static func == (lhs: LoggedExerciseByDayDTO, rhs: LoggedExerciseByDayDTO) -> Bool {
+        lhs.date == rhs.date
+    }
+
+    var date: String
+    var exercises: [LoggedExerciseDTO]
+    var durationInSeconds: Int
+
+    static func getDateFormat(for date: Date) -> String {
+        let iso = ISO8601DateFormatter()
+        let dateString = iso.string(from: Date())  // e.g. "2025-08-01T17:45:12Z"
+        return dateString
+    }
 }
