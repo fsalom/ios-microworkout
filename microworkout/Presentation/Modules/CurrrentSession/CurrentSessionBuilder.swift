786bbd686f2a3bc7e5ec36a803b3b1299d71e034
Fernando Salom <fer@rudo.es>
2025-08-04 15:21:43 +0200

    Replace normal injection with container

--- microworkout/Presentation/Modules/CurrrentSession/CurrentSessionBuilder.swift
+++ microworkout/Presentation/Modules/CurrrentSession/CurrentSessionBuilder.swift
@@ -2,13 +2,8 @@
 
 class CurrentSessionBuilder {
     func build() -> CurrentSessionView {
-        let loggedExerciseDataSource: LoggedExerciseDataSourceProtocol = LoggedExerciseMemoryDataSource()
-        let loggedExerciseRepository: LoggedExerciseRepositoryProtocol = LoggedExerciseRepository(dataSource: loggedExerciseDataSource)
-        let loggedExerciseUseCase: LoggedExerciseUseCaseProtocol = LoggedExerciseUseCase(repository: loggedExerciseRepository)
-        let mockExerciseDataSource: ExerciseDataSourceProtocol = ExerciseMockDataSource()
-        let exerciseRepository: ExerciseRepositoryProtocol = ExerciseRepository(dataSource: mockExerciseDataSource)
-        let exerciseUseCases: ExerciseUseCaseProtocol = ExerciseUseCase(repository: exerciseRepository)
-        let viewModel = CurrentSessionViewModel(exerciseUseCase: exerciseUseCases, loggedExerciseUseCase: loggedExerciseUseCase)
+        let viewModel = CurrentSessionViewModel(exerciseUseCase: ExerciseContainer().makeUseCase(),
+                                                loggedExerciseUseCase: LoggedExerciseContainer().makeUseCase())
         return CurrentSessionView(viewModel: viewModel)
     }
 }
