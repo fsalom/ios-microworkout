9a39061e8f8863f15e250828718cf9fb044f3ee5
Fernando Salom <fer@rudo.es>
2025-08-12 15:57:51 +0200

    Remove unnecessary files

--- microworkout/Data/Datasources/LoggedExercise/LoggedExerciseDataSourceProtocol.swift
+++
@@ -1,8 +0,0 @@
-protocol LoggedExerciseDataSourceProtocol {
-    func add(this exercise: LoggedExerciseDTO) async throws -> [LoggedExerciseDTO]
-    func update(this exercise: LoggedExerciseDTO) async throws -> [LoggedExerciseDTO]
-    func delete(with id: String) async throws -> [LoggedExerciseDTO]
-    func delete(this loggedExercisesByDay: LoggedExerciseByDay) async throws
-    func save(these exercises: [LoggedExerciseDTO], with duration: Int) async throws
-    func getAll() async throws -> [LoggedExerciseByDayDTO]
-}
