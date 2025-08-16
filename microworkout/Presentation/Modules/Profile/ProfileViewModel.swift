cda3a50a108bc7758fa630c763fbf98f22d1edcf
Fernando Salom <fer@rudo.es>
2025-08-16 19:10:37 +0200

    Update dependency

--- microworkout/Presentation/Modules/Profile/ProfileViewModel.swift
+++ microworkout/Presentation/Modules/Profile/ProfileViewModel.swift
@@ -1,22 +1,15 @@
-//
-//  CurrentSessionViewModel.swift
-//  microworkout
-//
-//  Created by Fernando Salom Carratala on 19/7/25.
-//
-
-
 import SwiftUI
 import Combine
 
 class ProfileViewModel: ObservableObject {
     private var exerciseUseCase: ExerciseUseCaseProtocol
-    private var loggedExerciseUseCase: LoggedExerciseUseCaseProtocol
+    private var workoutEntryUseCase: WorkoutEntryUseCaseProtocol
 
-    init(exerciseUseCase: ExerciseUseCaseProtocol, loggedExerciseUseCase: LoggedExerciseUseCaseProtocol) {
-        self.exerciseUseCase = exerciseUseCase
-        self.loggedExerciseUseCase = loggedExerciseUseCase
-    }
+init(exerciseUseCase: ExerciseUseCaseProtocol,
+     workoutEntryUseCase: WorkoutEntryUseCaseProtocol) {
+    self.exerciseUseCase = exerciseUseCase
+    self.workoutEntryUseCase = workoutEntryUseCase
+}
 
     
 }
