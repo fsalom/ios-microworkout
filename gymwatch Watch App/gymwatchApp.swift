00a0e8513e717d8fdc018d690fafea64e2e59589
Fernando Salom <fer@rudo.es>
2026-02-25 22:12:51 +0100

    Update gymwatchApp.swift

--- gymwatch Watch App/gymwatchApp.swift
+++ gymwatch Watch App/gymwatchApp.swift
@@ -5,12 +5,9 @@ struct gymwatch_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView(root: MenuBuilder().build())
-            //HeartBeatBuilder().build()
-            //ListPlanBuilder().build()
-            /*
-            TimerBuilder().build(this: Workout(exercise: Exercise(name: "example",
-                                                                  type: .distance),
-                                               results: [], serie: Serie(reps: 10, distance: 10.0)))*/
+                .onAppear {
+                    WatchConnectivityManager.shared.activate()
+                }
         }
     }
 }
