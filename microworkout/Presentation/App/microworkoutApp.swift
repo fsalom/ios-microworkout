fa9c5eaab4b9fbeeec34c03bb465da1e442bc48f
Fernando Salom <fer@rudo.es>
2026-02-26 18:00:55 +0100

    Update microworkoutApp.swift

--- microworkout/Presentation/App/microworkoutApp.swift
+++ microworkout/Presentation/App/microworkoutApp.swift
@@ -3,18 +3,27 @@ import SwiftUI
 @main
 struct MicroWorkoutApp: App {
     @StateObject var appState: AppState
+    @StateObject var mirrorManager = WorkoutMirrorManager.shared
 
     init() {
         let initialScreen: AppState.Screen = UserProfileContainer()
             .makeUseCase()
             .hasCompletedOnboarding() ? .home : .onboarding
         _appState = StateObject(wrappedValue: AppState(initialScreen: initialScreen))
+        // Register mirroring handler early
+        _ = WorkoutMirrorManager.shared
     }
 
     var body: some Scene {
         WindowGroup {
             RootView(root: SwitcherView())
                 .environmentObject(appState)
+                .environmentObject(mirrorManager)
+                .onAppear {
+                    _ = PhoneConnectivityManager.shared
+                    let trainings = TrainingContainer().makeUseCase().getTrainings()
+                    PhoneConnectivityManager.shared.sendTrainings(trainings)
+                }
         }
     }
 }
