a213fad957a5fb3c2697221b8ac2bd769900a7ce
Fernando Salom <fer@rudo.es>
2026-02-26 18:00:51 +0100

    Update Training.swift

--- microworkout/Domain/Entities/Training.swift
+++ microworkout/Domain/Entities/Training.swift
@@ -5,7 +5,7 @@ enum TrainingType: String, Codable {
     case strength
 }
 
-struct Training: Identifiable, Equatable {
+struct Training: Identifiable, Equatable, Codable {
     var id: UUID = UUID()
     var name: String
     var image: String
