d03e10effbc4193870ecc6aa700436e2d1ef1457
Fernando Salom <fer@rudo.es>
2026-03-03 17:59:40 +0100

    Create TrainingUseCaseProtocol.swift

---
+++ microworkout/Domain/UseCases/TrainingUseCaseProtocol.swift
@@ -0,0 +1,7 @@
+protocol TrainingUseCaseProtocol {
+    func getTrainings() -> [Training]
+    func getCurrent() -> Training?
+    func saveCurrent(_ training: Training)
+    func finish(_ training: Training)
+    func getFinished() -> [Training]
+}
