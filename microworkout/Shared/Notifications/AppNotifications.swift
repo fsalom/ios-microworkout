d558c16777e087d5120791940f27d421da77fb88
Fernando Salom <fer@rudo.es>
2026-05-10 16:12:49 +0200

    Create AppNotifications.swift

---
+++ microworkout/Shared/Notifications/AppNotifications.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+extension Notification.Name {
+    static let mealsChanged = Notification.Name("app.mealsChanged")
+    static let workoutLogsChanged = Notification.Name("app.workoutLogsChanged")
+}
